399f6ae614b6a0e7daf71793dad4b6aca142db21
Rimma Shayokhmatova <Rimma.Shayokhmatova@x5.ru>
2022-03-23 12:47:06 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,37 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result string
+	var previoseLetter string
+	var test string
+	var count int
+	for _, ch := range str {
+		if unicode.IsDigit(ch) {
+			if previoseLetter == "" {
+				err := ErrInvalidString
+				return result, err
+			}
+			count, _ = strconv.Atoi(string(ch))
+			if count > 0 {
+				test = strings.Repeat(previoseLetter, count)
+				result = result + test
+			}
+			previoseLetter = ""
+		} else {
+			if previoseLetter != "" {
+				result = result + previoseLetter
+			}
+			previoseLetter = string(ch)
+		}
+	}
+	result = result + previoseLetter
+	return result, nil
 }
