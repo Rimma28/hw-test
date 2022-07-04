f38cbe8631f0832425e2005fd604ab2264723fbb
Rimma Shayokhmatova <Rimma.Shayokhmatova@x5.ru>
2022-07-04 14:46:43 +0300

    use sync/atomic

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,19 +2,23 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func (t Task) Do() error {
-	err := t()
-	if err != nil {
-		return err
-	}
-	return err
+type countError int32
+
+func (c *countError) inc() int32 {
+	return atomic.AddInt32((*int32)(c), 1)
+}
+
+func (c *countError) get() int32 {
+	return atomic.LoadInt32((*int32)(c))
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -22,42 +26,42 @@ func Run(tasks []Task, n, m int) error {
 	if len(tasks) == 0 {
 		return ErrErrorsLimitExceeded
 	}
-	errorChan := make(chan error, 50)
 	var wg sync.WaitGroup
 	i := 1
 	k := 1
+	var c countError
 	for _, t := range tasks {
-		if len(errorChan) >= m {
+		if c.get() >= int32(m) {
 			return ErrErrorsLimitExceeded
 		}
 		t := t
 		if i <= n {
-			errorChan = runGoroutine(&wg, t, errorChan)
+			runGoroutine(&wg, t, &c)
 			i++
 			k++
 		} else {
 			wg.Wait()
-			errorChan = runGoroutine(&wg, t, errorChan)
+			runGoroutine(&wg, t, &c)
 			i = 1
-			if len(errorChan) >= m {
+			if c.get() >= int32(m) {
+				fmt.Println(444)
 				return ErrErrorsLimitExceeded
 			}
 		}
 	}
 	wg.Wait()
-	close(errorChan)
 
 	return nil
 }
 
-func runGoroutine(wg *sync.WaitGroup, t Task, errorChan chan error) chan error {
+func runGoroutine(wg *sync.WaitGroup, t Task, c *countError) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		err := t()
 		if err != nil {
-			errorChan <- err
+			c.inc()
 		}
 	}()
-	return errorChan
+	return
 }
